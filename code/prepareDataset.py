3aca40e9c8822f93cb12c17a27e5be90f546858f
Michael Rosenberg <michaelrosenberg@Michaels-MacBook-Pro.local>
2016-03-26 21:47:00 -0400

    Added some new datasets produced by prepareDataset.py

--- code/prepareDataset.py
+++ code/prepareDataset.py
@@ -4,6 +4,7 @@
 #imports
 
 import cPickle
+import treeUtil
 #helpers
 
 #main function
@@ -17,12 +18,29 @@ def collapseDataset(givenDataset):
     collapsedDataset.extend(givenDataset[2])
     return collapsedDataset
 
+def extractWordsFromParseTree(givenPTree,vocabDict,vocabSet):
+    #Function that extracts words from a given parse tree recursively
+    #first check if it is a leaf (which is then a word)
+    if isinstance(givenPTree,treeUtil.leafObj):
+        #has a word
+        if (givenPTree.word in vocabSet):
+            #attach numerical value to it
+            givenPTree.alpha = vocabDict[givenPTree.word]
+        else : #need to add it to the set
+            vocabSet.add(givenPTree.word)
+            vocabDict[givenPTree.word] = len(vocabDict)
+            #and then relabel the word
+            givenPTree.alpha = vocabDict[givenPTree.word]
+    else: #we have a node object, recurse through its child nodes
+        extractWordsFromParseTree(givenPTree.c1,vocabDict,vocabSet)
+        extractWordsFromParseTree(givenPTree.c2,vocabDict,vocabSet)
+
 def generateVocabulary(givenDataset,vocabFilename,alteredDatasetFilename):
     vocabDict = {}
     vocabSet = set([]) #to make it easier to look up vocab words
     for parseTree in givenDataset:
         extractWordsFromParseTree(parseTree,vocabDict,vocabSet)
-        #then test some aspects of the parse tree
+        #testing this out
     #then dump the altered dataset and vocabulary
     cPickle.dump(givenDataset,open(alteredDatasetFilename,"wb"))
     cPickle.dump(vocabDict,open(vocabFilename,"wb"))
